0aa94f7507a7926bdb62aafeb7d0966ad12d907f
slinkydeveloper <me@slinkydeveloper.com>
2026-06-11 09:47:20 +0200

    Update librdkafka version to v2.14.2

--- rdkafka-sys/src/bindings.rs
+++ rdkafka-sys/src/bindings.rs
@@ -3,7 +3,7 @@
 use libc::{c_char, c_int, c_void, sockaddr, FILE};
 use num_enum::TryFromPrimitive;
 
-pub const RD_KAFKA_VERSION: i32 = 34341375;
+pub const RD_KAFKA_VERSION: i32 = 34472703;
 pub const RD_KAFKA_DEBUG_CONTEXTS : & [u8 ; 138] = b"all,generic,broker,topic,metadata,feature,queue,msg,protocol,cgrp,security,fetch,interceptor,plugin,consumer,admin,eos,mock,assignor,conf\0" ;
 pub const RD_KAFKA_DESTROY_F_NO_CONSUMER_CLOSE: i32 = 8;
 pub const RD_KAFKA_OFFSET_BEGINNING: i32 = -2;
